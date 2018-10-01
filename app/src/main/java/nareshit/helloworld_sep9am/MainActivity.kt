9cbf9d8051a6cb48d98e583482f5486501f62c6c
maheshthippala <mahesh.cubex@gmail.com>
2018-10-01 10:29:08 +0530

    Initial commit

---
+++ app/src/main/java/nareshit/helloworld_sep9am/MainActivity.kt
@@ -0,0 +1,11 @@
+package nareshit.helloworld_sep9am
+
+import android.os.Bundle
+
+class MainActivity:android.app.Activity()
+{
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
